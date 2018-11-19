87bfc68d6029224ad0d0c3ddfab78e61c85dc230
jeloneklibera <jan.piotrowski00@gmail.com>
2018-11-19 13:21:24 +0100

    ramki i Math round

--- frontend/src/app/model-details/model-details.component.ts
+++ frontend/src/app/model-details/model-details.component.ts
@@ -13,6 +13,7 @@ export class ModelDetailsComponent implements OnInit {
   private mnk: any;
   private headers: any;
   private loading = true;
+  private clicked = false;
   constructor(private upServ: UploadService, private router: Router) { }
 
   ngOnInit() {
@@ -24,10 +25,14 @@ export class ModelDetailsComponent implements OnInit {
   }
   getParam(header: string){
     const index=this.mnk.headers.indexOf(header);
-    return this.mnk.parameters[index+1]
+    return Math.round(this.mnk.parameters[index+1] *10000)/10000;
   }
   goToPredictions(){
     this.router.navigateByUrl("/predict")
+  }
+
+  more(){
+    this.clicked = true;
   }
 
 }
